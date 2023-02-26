9af8a36a75724470fe6311d8afd477cfc1e51a83
Aws-star <awsalrahim@yahoo.com>
2023-02-25 23:00:57 -0500

    Continuing

--- client.js
+++ client.js
@@ -3,14 +3,21 @@ const net = require("net");
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "10.10.10.159", // IP addre
+    host: "127.0.0.1", // IP addre
     port: 50541 // PORT number here,
   });
 
+  conn.on("connect", () => {
+    conn.write( "Name: A.A");
+  });
+
+  conn.on("connect", () => {
+    conn.write( "Move: up");
+  });
+  
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
   return conn;
 };
 
-  module.exports = connect;
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,4 +1,25 @@
 const connect = require("./client");
 
+const setupInput = function () {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+const handleUserInput = function () {
+  // your code here
+  console.log("Getting Inputs: ", data);
+};
+
+
 console.log("Connecting ...");
+
 connect();
+
+setupInput();
