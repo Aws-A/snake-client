2555c0829df88f7d16759eb2b88d25920fbea091
Aws-star <awsalrahim@yahoo.com>
2023-02-25 23:12:28 -0500

    Input Module

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const connect = require("./client");
 
-const setupInput = function () {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-}
-
-const handleUserInput = function () {
-  // your code here
-  console.log("Getting Inputs: ", data);
-};
-
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 
